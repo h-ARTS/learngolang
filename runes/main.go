4f4007168d2890ba02068737574c05b7f2d34794
h-ARTS <hi5hanan@yahoo.com>
2018-04-28 22:03:30 +0200

    string type

--- runes/main.go
+++ runes/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	for i := 100; i <= 1800; i++ {
+	for i := 100; i <= 180; i++ {
 		fmt.Println(i, " – ", string(i), " – ", []byte(string(i)))
 	}
+	foo := 'a' // 'a' <-- rune "a" <-- string
+	fmt.Println(foo)
+	fmt.Printf("%T \n", foo)
+	fmt.Printf("%T \n", rune(foo))
 }
